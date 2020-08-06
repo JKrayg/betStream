0e213a2b2d2a56878cdebe7004aae4ac40dc6a63
Jake Krayger <jkrayg@protonmail.com>
2020-08-06 11:16:24 -0400

    video chat works

--- client/src/components/Webcam/webcam.js
+++ client/src/components/Webcam/webcam.js
@@ -2,28 +2,42 @@ import React from 'react'
 import Peer from "simple-peer"
 
 function Webcam() {
-    window.onload=function(){
-        var peer = new Peer({
-        initiator: window.location.hash === "#init",
-        trickle: false
-        })
-        peer.on("signal", function (data) {
-            document.getElementById("yourId").value = JSON.stringify(data);
-        })
+    window.onload = function(){
+        navigator.webkitGetUserMedia({ video: true, audio: true}, function(stream) {
+            var peer = new Peer({
+                initiator: window.location.hash === "#init",
+                trickle: false,
+                stream: stream
+            })
+            peer.on("signal", function (data) {
+                document.getElementById("yourId").value = JSON.stringify(data);
+            })
 
-        document.getElementById("connect").addEventListener("click", function () {
-            var peerId = JSON.parse(document.getElementById("peerId").value)
-            peer.signal(peerId)
-        })
+            document.getElementById("connect").addEventListener("click", function () {
+                var peerId = JSON.parse(document.getElementById("peerId").value)
+                peer.signal(peerId)
+            })
 
-        document.getElementById("send").addEventListener("click", function () {
-            var yourMessage = document.getElementById("yourMessage").value
-            peer.send(yourMessage)
-        })
+            document.getElementById("send").addEventListener("click", function () {
+                var yourMessage = document.getElementById("yourMessage").value
+                peer.send(yourMessage)
+            })
 
-        peer.on("data", function(data) {
-            document.getElementById("messages").textContent += data
-        })
+            peer.on("data", function(data) {
+                document.getElementById("messages").textContent += data + "\n"
+            })
+
+            peer.on("stream", function (stream) {
+                var root = document.getElementById("root")
+                var video = document.createElement("video");
+                root.appendChild(video)
+
+                video.srcObject = stream;
+                video.play();
+            })
+        }, function (err) {
+            console.log(err)
+        });
     }
     
 
